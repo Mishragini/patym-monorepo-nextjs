f99ec79e77383f5cb10ce30ffd46cc5762b0c5f3
Mishragini <mishragini11.com>
2024-04-03 12:30:13 +0530

    attempt 1

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
+//test attempt 1 for CI
+
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
