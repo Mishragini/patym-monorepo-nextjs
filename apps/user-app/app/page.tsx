840b916f07b973ecf5d99169345cc4eb5a78cf85
Mishragini <mishragini11.com>
2024-04-03 12:39:25 +0530

    attempt2

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,6 +4,8 @@ import { authOptions } from "./lib/auth";
 
 //test attempt 1 for CI
 
+//test attemp2 for CT
+
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
