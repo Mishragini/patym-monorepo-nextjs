6188f4611251d6df54faf85c9c5c947abfa06ed8
Mishragini <mishragini11.com>
2024-04-08 20:39:46 +0530

    adds next-auth.d.ts

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -14,12 +14,12 @@ interface AuthUser {
   number: string
 }
 interface CustomSession extends Session {
-    user: {
-        id: string;
-        name?: string | null | undefined;
-        email?: string | null | undefined;
-        image?: string | null | undefined;
-      };
+     user: {
+    id: string;
+    name?: string | null | undefined;
+    email?: string | null | undefined;
+    image?: string | null | undefined;
+  };
   }
 
 export const authOptions = {
